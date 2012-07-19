9b5e43299be918f70b7f4151e06eaaa1a45972c7
adefanti <aurelien.defanti@gmail.com>
2012-07-19 02:22:13 +0200

    define interface for Customer Session Facade

---
+++ yaps/yaps-ejb/src/main/java/org/yaps/petstore/service/facade/customer/CustomerFacade.java
@@ -0,0 +1,10 @@
+package org.yaps.petstore.service.facade.customer;
+
+import org.yaps.petstore.domain.model.customer.Customer;
+import org.yaps.petstore.service.common.Facade;
+
+/**
+ * Interface defines all functions for Session Facade for Customer domain. 
+ */
+public interface CustomerFacade extends Facade<Customer> {
+}
