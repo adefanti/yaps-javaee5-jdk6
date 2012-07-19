806e68d6f99b5c449643c7c5e47ce463f782ab95
adefanti <aurelien.defanti@gmail.com>
2012-07-19 02:04:30 +0200

    create AbstractFacade Session Facade inherit from this class

---
+++ yaps/yaps-ejb/src/main/java/org/yaps/petstore/service/common/Facade.java
@@ -0,0 +1,45 @@
+package org.yaps.petstore.service.common;
+
+/**
+ * Class defines base service functions.
+ * 
+ * @param <T>
+ *            - type of the entity
+ */
+public interface Facade<T> {
+
+	/**
+	 * Insert new entity on <T> type in database.
+	 * 
+	 * @param entity
+	 *            - entity to create
+	 * @return created entity
+	 */
+	T create(T entity);
+
+	/**
+	 * Update entity on <T> type in database.
+	 * 
+	 * @param entity
+	 *            - entity to update
+	 * @return updated entity
+	 */
+	T update(T entity);
+
+	/**
+	 * Remove entity on <T> type in database.
+	 * 
+	 * @param entity
+	 *            - entity to remove
+	 */
+	void delete(T entity);
+
+	/**
+	 * Find entity by primary key
+	 * 
+	 * @param id
+	 *            - primary key object
+	 * @return found entity
+	 */
+	T find(Object id);
+}
